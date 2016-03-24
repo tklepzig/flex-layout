686ab511a3e7f9c2eb2d1796c0c7cb52be12e99b
Thomas Klepzig <thomas.klepzig@comparex.de>
2016-03-24 10:22:06 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({
+    pattern: '*',
+    scope: ['dependencies', 'devDependencies']
+});
+
+gulp.task('sass', function() {
+    return gulp.src('flex-layout.scss')
+        .pipe(plugins.sass().on('error', plugins.util.log))
+        .pipe(plugins.autoprefixer({
+            cascade: false
+        }))
+        .pipe(plugins.rename('flex-layout.css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(plugins.cssnano())
+        .pipe(plugins.rename('flex-layout.min.css'))
+        .pipe(gulp.dest('./dist'));
+});
+gulp.task('watch:sass', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('dev', ['sass', 'watch:sass']);
+gulp.task('default', ['sass']);
