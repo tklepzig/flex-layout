316b335a087bc2ffd4989222ccbc5661ce3a9258
Thomas <Thomas@DevBook>
2016-06-05 13:20:54 +0200

    removed old files

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var plugins = require('gulp-load-plugins')({
 });
 
 gulp.task('sass', function() {
-    return gulp.src('scss/flex-layout-new.scss')
+    return gulp.src('scss/flex-layout.scss')
         .pipe(plugins.sass().on('error', plugins.util.log))
         .pipe(plugins.autoprefixer({
             cascade: false
